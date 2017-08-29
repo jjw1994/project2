f3c975415ddb8c870197bc2bfa9ff04f1eed681a
jjw1994 <15932023781@163.com>
2017-08-29 20:50:35 +0800

    personWeb

---
+++ js/xc.js
@@ -0,0 +1,59 @@
+var timer, m = 0, m1 = $("img[rel='lazy']").length;
+
+    function fade() {
+
+        $("img[rel='lazy']").each(function () {
+
+            var $scroTop = $(this).offset();
+
+            if ($scroTop.top <= $(window).scrollTop() + $(window).height()) {
+
+                $(this).hide();
+
+                $(this).attr("src", $(this).attr("lazy_src"));
+
+                $(this).attr("top", $scroTop.top);
+
+                $(this).removeAttr("rel");
+
+                $(this).removeAttr("lazy_src");
+
+                $(this).fadeIn(600);
+
+                var _left = $(this).parent().parent().attr("_left");
+
+                if (_left != undefined)
+
+                    $(this).parent().parent().animate({ left: _left }, 400);
+
+                m++;
+
+            }
+
+        });
+
+        if (m < m1) { timer = window.setTimeout(fade, 300); }
+
+        else { window.clearTimeout(timer); }
+
+    }
+
+    $(function () {
+
+        $("#wf-main img[rel='lazy']").each(function (i) {
+
+            var _left = $(this).parent().parent().css("left").replace("px", "");
+
+            $(this).parent().parent().attr("_left", _left);
+
+            $(this).parent().parent().css("left", 0);
+
+        });
+
+        fade();
+
+    });
+
+    $(".loading").hide();
+
+    $("#wf-main").show();
